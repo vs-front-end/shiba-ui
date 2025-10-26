e666858a3271632bffecf881cf172885d2ca48c9
vs-front-end <contato.soer@gmail.com>
2025-10-26 15:52:24 -0300

    📄 docs: change Button component category

--- packages/docs/stories/browser/Button.stories.tsx
+++ packages/docs/stories/browser/Button.stories.tsx
@@ -2,7 +2,7 @@ import { Button } from "@shiba-ui/browser";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof Button> = {
-  title: "WEB - Components/Buttons/Button",
+  title: "WEB - Components/Form/Button",
   component: Button,
 };
 
