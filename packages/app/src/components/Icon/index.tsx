66aecb73ec527e30dfc6a7b1eb90ca119383cce4
vs-front-end <contato.soer@gmail.com>
2025-11-08 16:52:12 -0300

    feat: Add Icon component with customizable properties and integration with Feather icons

---
+++ packages/app/src/components/Icon/index.tsx
@@ -0,0 +1,16 @@
+import { IIcon } from '@shiba-ui/shared';
+import { Feather } from '@expo/vector-icons';
+import { useTheme } from 'styled-components/native';
+
+export const Icon = ({
+  icon = 'award',
+  size = 24,
+  color = 'content',
+  isHidden = false,
+}: IIcon) => {
+  const { colors } = useTheme();
+
+  if (isHidden) return null;
+
+  return <Feather name={icon} size={size} color={colors[color]} />;
+};
