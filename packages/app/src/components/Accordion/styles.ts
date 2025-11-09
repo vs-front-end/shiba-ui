009d0babb9db7cf0390411d9cd60e6e63157f589
vs-front-end <contato.soer@gmail.com>
2025-11-09 10:46:36 -0300

    feat: Add Accordion component with animations and styled elements

---
+++ packages/app/src/components/Accordion/styles.ts
@@ -0,0 +1,51 @@
+import styled, { css } from 'styled-components/native';
+import { IAccordion } from '@shiba-ui/shared';
+
+export const Container = styled.View<IAccordion>`
+  ${({
+    theme,
+    background,
+    borderColor,
+    width,
+    borderRadius,
+    borderWidth,
+  }) => css`
+    overflow: hidden;
+
+    width: ${width ? `${width}px` : '100%'};
+    background: ${(background && theme.colors[background]) || 'transparent'};
+
+    border-style: solid;
+    border-width: ${borderWidth ? `${borderWidth}px` : '1px'};
+    border-radius: ${borderRadius ? `${borderRadius}px` : '8px'};
+    border-color: ${theme.colors[borderColor || 'highlight']};
+  `}
+`;
+
+export const Header = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 16px 20px;
+
+  background: transparent;
+`;
+
+export const TitleContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const Content = styled.View<{ hasIcon?: boolean }>`
+  ${({ hasIcon }) => css`
+    width: 100%;
+    padding: ${hasIcon ? '0 48px 16px 48px' : '0 36px 16px 20px'};
+  `}
+`;
