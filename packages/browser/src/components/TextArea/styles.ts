248e59e298a0b614f992e351852c62b30094673c
vs-front-end <contato.soer@gmail.com>
2025-12-14 14:40:04 -0300

    refactor: Remove focus-visible styles from TextArea component for cleaner appearance

--- packages/browser/src/components/TextArea/styles.ts
+++ packages/browser/src/components/TextArea/styles.ts
@@ -44,11 +44,6 @@ export const TextArea = styled.textarea<ITextArea>`
       outline: none;
     }
 
-    &:focus-visible {
-      outline: 2px solid ${({ theme }) => theme.colors.primary};
-      outline-offset: 2px;
-    }
-
     &:disabled {
       cursor: not-allowed;
       opacity: 0.5;
